8bedd774b716364feef62287b90c181019c711dd
Yanyeyeyes <1024079655@qq.com>
2023-07-01 19:23:45 +0800

    chore: :pencil: 修改github文章路径

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
   },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/Yanyeyeye' }
+      { icon: 'github', link: 'https://github.com/Yanyeyeye/Learning-Note' }
     ],
 
     footer: {
